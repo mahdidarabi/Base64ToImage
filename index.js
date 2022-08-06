586d691bde7b162f078a681000a1cf0c220a6c25
Mahdi Darabi <mahdidarabi7@yahoo.com>
2022-08-07 01:40:15 +0430

    Add functions

---
+++ index.js
@@ -0,0 +1,31 @@
+data = require("./data.js");
+var fs = require("fs");
+
+const makeOutDir = (outDirPath = "./out/") => {
+  if (!fs.existsSync(outDirPath)) {
+    fs.mkdirSync(outDirPath, { recursive: true });
+  }
+};
+
+const generateImageFiles = (data, fileName, outDirPath = "./out/") => {
+  var base64Data = data; // grid & constellations & base
+
+  fs.writeFile(
+    `${outDirPath}${fileName}.png`,
+    base64Data,
+    "base64",
+    function (err) {
+      console.log(err ?? "ok!");
+    }
+  );
+};
+
+const main = () => {
+  makeOutDir();
+
+  for (var key in data) {
+    generateImageFiles(data[key], key);
+  }
+};
+
+main();
